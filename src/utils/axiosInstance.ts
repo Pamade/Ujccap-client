b20d28bef6b48b5bd0e9a57bd9c1e35ce2e5f3d9
Pamade <insune15@gmail.com>
2023-02-11 14:42:59 +0100

    fix

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE = 'https://ujccap-server-production.up.railway.app/' as const
+const BASE = 'https://ujccap-server-production.up.railway.app' as const
 const axiosInstance = (authNeeded = false, token:string) => {
   return axios.create({
     baseURL: `${BASE}/api/v1`,
